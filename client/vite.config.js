0dc6ead2aaa87a667bc9d669a25781bc26ebd240
sangeetha <geethastockholm@gmail.com>
2026-09-21 21:28:12 +0200

    Configured Vite and added index.html ,Client

---
+++ client/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    proxy: {
+      "/api": "http://localhost:3000",
+    },
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./src/setupTests.js",
+    coverage: { reporter: ["text", "lcov"] },
+  },
+});
